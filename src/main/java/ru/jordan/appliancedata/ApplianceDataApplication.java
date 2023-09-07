140a466daa508983f69997116ddee080d868ce95
Igor <igorjordan210@gmail.com>
2023-09-07 11:14:21 +0300

    Start

---
+++ src/main/java/ru/jordan/appliancedata/ApplianceDataApplication.java
@@ -0,0 +1,13 @@
+package ru.jordan.appliancedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplianceDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplianceDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jordan/appliancedata/ApplianceDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jordan.appliancedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplianceDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
