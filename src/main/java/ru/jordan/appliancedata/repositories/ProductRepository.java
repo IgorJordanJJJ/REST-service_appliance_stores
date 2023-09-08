c4c74e053f43a609320b436579cc90dc7446472f
Igor <igorjordan210@gmail.com>
2023-09-09 00:41:03 +0300

    Add in Repositories multifilter search

--- src/main/java/ru/jordan/appliancedata/repositories/ProductRepository.java
+++ src/main/java/ru/jordan/appliancedata/repositories/ProductRepository.java
@@ -3,7 +3,15 @@ package ru.jordan.appliancedata.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.jordan.appliancedata.models.Product;
+import ru.jordan.appliancedata.models.Showcases;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findByType(String type);
+    List<Product> findByPriceBetween(BigDecimal onprice, BigDecimal  toprice);
+    List<Product> findByShowcases(Showcases showcases);
+
 }
--- src/main/java/ru/jordan/appliancedata/repositories/ShowcasesRepository.java
+++ src/main/java/ru/jordan/appliancedata/repositories/ShowcasesRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.jordan.appliancedata.models.Showcases;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface ShowcasesRepository extends JpaRepository<Showcases, Integer> {
+    List<Showcases> findByType(String storeType);
+    List<Showcases> findByAddress(String address);
+    List<Showcases> findByDatecreateBetween(Date startDate, Date endDate);
+    List<Showcases> findBydatechangeBetween(Timestamp startDate, Timestamp endDate);
 }
